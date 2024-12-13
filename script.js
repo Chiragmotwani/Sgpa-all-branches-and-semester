986e0040cf488eb5db54cf40e584c542f05c9d6f
Chirag Motwani <128508362+Chiragmotwani@users.noreply.github.com>
2024-12-13 20:28:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-        function redirectToPage() {
-            var branch = document.getElementById("branch").value;
-            var semester = document.getElementById("semester").value;
-            var destination = branch + "/" + semester +  "/index.html";
-            document.getElementById("branchSemesterForm").action = destination;
-        }
-
-        
