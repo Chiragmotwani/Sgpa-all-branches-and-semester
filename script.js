56147dac4427ecd1725674eac3ce74dcdba56a6c
Chirag Motwani <128508362+Chiragmotwani@users.noreply.github.com>
2024-12-13 20:18:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
         function redirectToPage() {
             var branch = document.getElementById("branch").value;
             var semester = document.getElementById("semester").value;
-            var destination = branch + "/" + semester + "/index.html";
+            var destination = branch + "/" + semester + "index.html";
             document.getElementById("branchSemesterForm").action = destination;
         }
 
