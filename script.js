5152de0243e397c12a160dc67ffae82f46a5a232
Chirag Motwani <128508362+Chiragmotwani@users.noreply.github.com>
2024-12-13 18:22:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+        function redirectToPage() {
+            var branch = document.getElementById("branch").value;
+            var semester = document.getElementById("semester").value;
+            var destination = branch + "/" + semester + "/index.html";
+            document.getElementById("branchSemesterForm").action = destination;
+        }
+
+        
